8bba70593ef8873c92e6dc9589f67eac614f0f32
davidstev <david.stevenson@arnoldclark.com>
2023-07-31 07:48:19 +0100

    Initial Commit
    Initialcommit of example CRUD Azure Blob Storage app.

---
+++ BlobStorage/BlobStorage/AzureBlobStorage.cs
@@ -0,0 +1,108 @@
+﻿using Azure.Storage.Blobs;
+
+// Azure SDK
+// BlobServiceClient (Storage Account) used to acccess BlobContainerClient (Folders) Which can then access the BlobClient (Files)
+
+namespace BlobStorage
+{
+    internal class AzureBlobStorage
+    {
+
+        private readonly string _storageAccount = "";
+        // _accessKey this is the most insecure method of authentication where a key is stored in vault and passed to authenticate.
+        // Also available is
+        
+        // Azure AD
+        //Create Service Principle to access Storage Account.You need to register application.
+
+       //Shared Access Signatures
+       //Allows granular access to storage account.Access to specific areas, for specific people, for specific time.
+        private readonly string _accessKey = "";
+        private readonly BlobServiceClient _blobServiceClient;
+        private const string ContainerName = "files";
+        private const string TodayFile = "test.txt";
+        private const string TomorrowFile = "test.pdf";
+        private const string TodayBlobPath = $"today/{TodayFile}";
+        private const string TomorrowBlobPath = $"tomorrow/{TomorrowFile}";
+        public AzureBlobStorage()
+        {
+            var credential = new Azure.Storage.StorageSharedKeyCredential(_storageAccount, _accessKey);
+            var blobUrl = $"https://{_storageAccount}.blob.core.windows.net";
+            _blobServiceClient = new BlobServiceClient(new Uri(blobUrl), credential);
+
+        }
+
+        public async Task ListBlobContainersAsync()
+        {
+            var containers = _blobServiceClient.GetBlobContainersAsync();
+
+            await foreach(var container in containers)
+            {
+                Console.WriteLine(container.Name);
+            }
+        }
+
+        public async Task<List<Uri>> UploadFilesAsync()
+        {
+            var blobUris = new List<Uri>();
+
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+            // TodayBlobPath = "today/test.txt"
+            var todayBlobClient = blobContainerClient.GetBlobClient(TodayBlobPath);
+            // TomorrowBlobPath = "tomorrow/test.pdf"
+            var tomorrowBlobClient = blobContainerClient.GetBlobClient(TomorrowBlobPath);
+
+            // TodayFile physical path to file on disk
+            // true = override if file already exists, false will result in exception being thrown
+            await todayBlobClient.UploadAsync(TodayFile, true);
+            blobUris.Add(todayBlobClient.Uri);
+
+            await tomorrowBlobClient.UploadAsync(TomorrowFile, true);
+            blobUris.Add(tomorrowBlobClient.Uri);
+
+            return blobUris;
+        }
+
+        public async Task GetFlatBlobListAsync()
+        {
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+            var blobs = blobContainerClient.GetBlobsAsync();
+
+            await foreach(var blob in blobs)
+            {
+                Console.WriteLine($"Blob Name = {blob.Name}");
+            }
+        }
+
+        public async Task GetHierarchialBlobListAsync()
+        {
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+            var blobs = blobContainerClient.GetBlobsByHierarchyAsync();
+
+            await foreach (var blob in blobs)
+            {
+                if(blob.IsPrefix)
+                {
+                    // Write out the prefix of the Virtual Dirctory
+                    Console.WriteLine($"Virtual Directory Prefix {blob.Prefix}");
+
+                    // Call Recursively With The Prefix To Traverse The Virtual Directory
+                    await GetFlatBlobListAsync();
+                }
+                else
+                {
+                    Console.WriteLine($"Blob Name = {blob.Prefix}");
+                }
+            }
+        }
+
+        public async Task DeleteFileAsync()
+        {
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(ContainerName);
+            var blob = blobContainerClient.GetBlobClient(TodayBlobPath);
+
+            await blob.DeleteIfExistsAsync();
+        }
+
+    }
+}
---
+++ BlobStorage/BlobStorage/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BlobStorage;
+
+var blobService = new AzureBlobStorage();
+await blobService.ListBlobContainersAsync();
+await blobService.UploadFilesAsync();
+await blobService.GetFlatBlobListAsync();
+Console.WriteLine("************");
+await blobService.GetHierarchialBlobListAsync();
+await blobService.DeleteFileAsync();
+
+Console.ReadLine();
